462dd2694ceb4baf2b88c81ddc9b8d4f4d62d102
WizarDiger <WizarDigeR@gmail.com>
2022-04-24 18:24:16 +0300

    v0.0.8 registation sends data to DB

--- DiplomFrontendReact/src/RegisterPage.js
+++ DiplomFrontendReact/src/RegisterPage.js
@@ -11,6 +11,7 @@ import LocationCityIcon from '@mui/icons-material/LocationCity';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { Alert } from '@mui/material';
 
 
 
@@ -20,9 +21,56 @@ export class RegisterPage extends Component {
         this.handleSubmit=this.handleSubmit.bind(this);
        
       }
+      state = { 
+		login: '', 
+        password:'',
+        email:'',
+        name:'',
+        surname:'',
+        patronymic:'',
+        dateOfBirth:'',
+        city:''
+	} 
+    handleLoginChange = (e) => this.setState({ 
+        login: e.target.value    
+	}) 
+    handlePasswordChange = (e) => this.setState({ 
+        password: e.target.value  
+   
+	}) 
+    handleMailChange = (e) => this.setState({ 
+        email: e.target.value  
+   
+	}) 
+    handleNameChange = (e) => this.setState({ 
+        name: e.target.value  
+   
+	}) 
+    handleSurnameChange = (e) => this.setState({ 
+        surname: e.target.value  
+   
+	}) 
+    handlePatronymicChange = (e) => this.setState({ 
+        patronymic: e.target.value  
+   
+	}) 
+    handleDateOfBirthChange = (e) => this.setState({ 
+        dateOfBirth: e.target.value  
+   
+	}) 
+    handleCityChange = (e) => this.setState({ 
+        city: e.target.value  
+   
+	}) 
+    handlePasswordCheck = (e) => this.setState({ 
+        city: e.target.value  
+   
+	}) 
+
+  
       handleSubmit(event)
       {
-         
+        
           event.preventDefault();
           fetch('https://localhost:7049/api/Login',{
               method:'POST',
@@ -31,15 +79,9 @@ export class RegisterPage extends Component {
                   'Accept':'application/json',
                   'Content-Type':'application/json'
               },
-              body:JSON.stringify({
-                  login:event.target.login.value,
-                  password:event.target.password.value,
-                  name:event.target.name.value,
-                  mail:event.target.mail.value,
-                  birthday:event.target.birthday.value
-
-              })
+              body:JSON.stringify(this.state)
           })
+          
           .then(res=>res.json())
           .then((result)=>{
               alert(result);
@@ -66,21 +108,21 @@ export class RegisterPage extends Component {
                     }}
                 >
                     <Box sx={{ mr: -4, my: 3, fontSize: 35 }}>
-                        Авторизация
+                        Регистрация
                     </Box>
                     <Box>
 
                         <AccountCircle sx={{ mr: 1, my: 3 }} />
-                        <TextField sx={{ my: 1 }}
+                        <TextField value={this.state.login} onChange={this.handleLoginChange} sx={{ my: 1 }}
                             id="outlined-password-input"
                             label="Логин"
-
+                            
                         />
                     </Box>
                     <Box>
 
                         <KeyIcon sx={{ mr: 1, my: 3 }} />
-                        <TextField sx={{ my: 1 }}
+                        <TextField value={this.state.password} onChange={this.handlePasswordChange} sx={{ my: 1 }}
                             id="outlined-password-input"
                             label="Пароль"
                             type="password"
@@ -90,7 +132,7 @@ export class RegisterPage extends Component {
                     <Box>
 
                         <KeyIcon sx={{ mr: 1, my: 3 }} />
-                        <TextField sx={{ my: 1 }}
+                        <TextField  sx={{ my: 1 }}
                             id="outlined-password-input"
                             label="Повторите пароль"
                             type="password"
@@ -99,7 +141,7 @@ export class RegisterPage extends Component {
                     <Box>
 
                         <EmailIcon sx={{ mr: 1, my: 3 }} />
-                        <TextField sx={{ my: 1 }}
+                        <TextField value={this.state.email} onChange={this.handleMailChange} sx={{ my: 1 }}
                             id="outlined-password-input"
                             label="Почта"
 
@@ -109,7 +151,7 @@ export class RegisterPage extends Component {
                     <Box>
 
                         <BadgeIcon sx={{ mr: 1, my: 3 }} />
-                        <TextField sx={{ my: 1 }}
+                        <TextField value={this.state.name} onChange={this.handleNameChange} sx={{ my: 1 }}
                             id="outlined-password-input"
                             label="Имя"
 
@@ -118,7 +160,7 @@ export class RegisterPage extends Component {
                     <Box>
 
                         <BadgeIcon sx={{ mr: 1, my: 3 }} />
-                        <TextField sx={{ my: 1 }}
+                        <TextField value={this.state.surname} onChange={this.handleSurnameChange} sx={{ my: 1 }}
                             id="outlined-password-input"
                             label="Фамилия"
 
@@ -127,7 +169,7 @@ export class RegisterPage extends Component {
                     <Box>
 
                         <BadgeIcon sx={{ mr: 1, my: 3 }} />
-                        <TextField sx={{ my: 1 }}
+                        <TextField value={this.state.patronymic} onChange={this.handlePatronymicChange} sx={{ my: 1 }}
                             id="outlined-password-input"
                             label="Отчество (при наличии)"
 
@@ -140,7 +182,7 @@ export class RegisterPage extends Component {
                        
                            
                         <CakeIcon sx={{ mr: 1, my: 3 }} />
-                        <TextField sx={{ my: 1 }}
+                        <TextField value={this.state.dateOfBirth} onChange={this.handleDateOfBirthChange} sx={{ my: 1 }}
                             id="outlined-password-input"
                             label="Дата рождения"
 
@@ -151,10 +193,10 @@ export class RegisterPage extends Component {
                     <Box>
 
                         <LocationCityIcon sx={{ mr: 1, my: 3 }} />
-                        <TextField sx={{ my: 1 }}
+                        <TextField value={this.state.city} onChange={this.handleCityChange} sx={{ my: 1 }}
                             id="outlined-password-input"
                             label="Город"
-                            type="password"
+                           
                         />
                     </Box>
                     <Button onClick={this.handleSubmit} sx={{ mr: -4, my: 2, fontSize: 20, width: 270 }} variant="contained">Зарегистрироваться</Button>
